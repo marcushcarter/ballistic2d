2559c1d560532ba4a1dbcba2d8a4a090a56069f7
Marcus Carter <marcoose408@gmail.com>
2026-06-23 10:14:27 -0400

    feat: image device driver opengl

--- drivers/opengl/rendering_device_driver_opengl.cpp
+++ drivers/opengl/rendering_device_driver_opengl.cpp
@@ -34,4 +34,78 @@ void RenderingDeviceDriverOpengl::shutdown()
     
 }
 
+/***************/
+/**** IMAGE ****/
+/***************/
+
+void RenderingDeviceDriverOpengl::_image_release(Image& r_image)
+{
+    if (r_image.storage_handle) {
+        glMakeImageHandleNonResidentARB(r_image.storage_handle);
+        r_image.storage_handle = 0;
+    }
+    
+    if (r_image.sampled_handle) {
+        glMakeTextureHandleNonResidentARB(r_image.sampled_handle);
+        r_image.sampled_handle = 0;
+    }
+    
+    if (r_image.image) {
+        glDeleteTextures(1, &r_image.image);
+        r_image.image = 0;
+    }
+}
+
+RenderingDeviceDriverOpengl::Image RenderingDeviceDriverOpengl::image_create(uint32_t p_width, uint32_t p_height, GLenum p_format)
+{
+    Image image;
+
+    image.width = p_width;
+    image.height = p_height;
+    image.format = p_format;
+
+    glCreateTextures(GL_TEXTURE_2D, 1, &image.image);
+    glTextureStorage2D(image.image, 1, image.format, static_cast<GLsizei>(image.width), static_cast<GLsizei>(image.height));
+    glTextureParameteri(image.image, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTextureParameteri(image.image, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTextureParameteri(image.image, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(image.image, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    image.sampled_handle = glGetTextureHandleARB(image.image);
+    glMakeTextureHandleResidentARB(image.sampled_handle);
+
+    image.storage_handle = glGetImageHandleARB(image.image, 0, GL_FALSE, 0, image.format);
+    glMakeImageHandleResidentARB(image.storage_handle, GL_READ_WRITE);
+
+    return image;
+}
+
+void RenderingDeviceDriverOpengl::image_free(Image& r_image)
+{
+    _image_release(r_image);
+}
+
+Error RenderingDeviceDriverOpengl::image_resize(Image& r_image, uint32_t p_width, uint32_t p_height)
+{
+    using enum Error;
+
+    if (r_image.width == p_width && r_image.height == p_height) return Ok;
+    if (p_width == 0 || p_height == 0) return Ok;
+
+    _image_release(r_image);
+    r_image = image_create(p_width, p_height, r_image.format);
+
+    return Ok;
+}
+
+void RenderingDeviceDriverOpengl::image_bind_sampled(Image& r_image, uint32_t p_unit)
+{
+    glBindTextureUnit(p_unit, r_image.image);
+}
+
+void RenderingDeviceDriverOpengl::image_bind_storage(Image& r_image, uint32_t p_unit, GLenum p_access)
+{
+    glBindImageTexture(p_unit, r_image.image, 0, GL_FALSE, 0, p_access, r_image.format);
+}
+    
 }
--- drivers/opengl/rendering_device_driver_opengl.h
+++ drivers/opengl/rendering_device_driver_opengl.h
@@ -15,6 +15,35 @@ struct RenderingDeviceDriverOpengl
 
     Error initialize();
     void shutdown();
+    
+    /***************/
+    /**** IMAGE ****/
+    /***************/
+
+    struct Image {
+        uint32_t image = 0;
+        uint64_t storage_handle = 0;
+        uint64_t sampled_handle = 0;
+        uint32_t width = 0;
+        uint32_t height = 0;
+        GLenum format = GL_RGBA8_SNORM;
+    };
+
+    void _image_release(Image& r_image);
+
+    Image image_create(uint32_t p_width, uint32_t p_height, GLenum p_format);
+    void image_free(Image& r_image);
+    Error image_resize(Image& r_image, uint32_t p_width, uint32_t p_height);
+    void image_bind_sampled(Image& r_image, uint32_t p_unit = 0);
+    void image_bind_storage(Image& r_image, uint32_t p_unit = 0, GLenum p_access = GL_READ_WRITE);
+    
+    /*********************/
+    /**** FRAMEBUFFER ****/
+    /*********************/
+    
+    /****************/
+    /**** SHADER ****/
+    /****************/
 };
 
 }
